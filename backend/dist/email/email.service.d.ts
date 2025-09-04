29304eb24b0460a5acb4ffdd6bce85a4f42be470
PabPol <pabpolval@gmail.com>
2025-09-04 00:54:11 -0400

    chore: remove compiled files from tracking and update lock file

--- backend/dist/email/email.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { ConfigService } from '@nestjs/config';
-export declare class EmailService {
-    private readonly configService;
-    private transporter;
-    private emailEnabled;
-    constructor(configService: ConfigService);
-    sendConfirmationEmail(email: string): Promise<void>;
-    private getEmailTemplate;
-}
--- backend/dist/export/export.service.d.ts
+++
@@ -1,4 +0,0 @@
-import { GuestResponseDto } from '../guest/dto/guest.dto';
-export declare class ExportService {
-    exportGuestsToExcel(guests: GuestResponseDto[]): Promise<void>;
-}
